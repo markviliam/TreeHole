2827aa962936054a2a0532577a5125c4da888bb6
Administrator <1559521084@qq.com>
2018-11-07 23:02:25 +0800

    初步版本

--- treehole/src/main/java/com/hny/treehole/login/controller/LoginController.java
+++ treehole/src/main/java/com/hny/treehole/login/controller/LoginController.java
@@ -1,5 +1,28 @@
 package com.hny.treehole.login.controller;
 
-public class LoginController {
+import java.io.IOException;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("login")
+public class LoginController {
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("加载成功");
+	}
+	
+	@RequestMapping(value = {"/test"}, method = RequestMethod.GET)
+	public @ResponseBody String test(HttpServletResponse response, HttpServletRequest request) throws IOException {
+		return "测试";
+	}
+	
 }
